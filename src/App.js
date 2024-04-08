f7c17d658cf0b3e6e92949e4659d619ad3bde86d
Raf4554 <https://github.com/Raf4554/Intensiv.git>
2024-04-08 15:56:57 +0400

    propsner clasneriev modulner  modulner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Card from './components/Card'
+import Card from './components/Card/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
+
+const arr = [
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "12 999 руб.", imageUrl:'/img/sneakers/1.jpeg'},
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: "12 999 руб.", imageUrl:'/img/sneakers/2.jpeg'},
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "8 499 руб.", imageUrl:'/img/sneakers/3.jpeg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: "8 999 руб.", imageUrl:'/img/sneakers/4.jpeg'}
+];
+
 function App() {
   return (
     <div className="wrapper clear">
-      <Header/>
       <Drawer/>
+      <Header/>
       <div className="content p-40 ">
         <div className="d-flex mb-40 align-center  justify-between">
           <h1>Все кроссовки</h1>
@@ -16,10 +24,11 @@ function App() {
         </div>
 
         <div className="d-flex">
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
+
+        {arr.map((obj) => ((
+        <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)} />
+        )))}
+
         </div>
 
       </div>
